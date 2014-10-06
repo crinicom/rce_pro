b96d90fc36181d27822354447dbfc041fb7cc9b5
crinicom <cristian.menajovsky@gmail.com>
2014-10-06 15:59:08 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RCEPro::Application.config.secret_key_base = '74e1791aa83b075ea1d610af69f58081dde7147c5222aefe2275b32df959e2be97a25905fe7a8f99f40e9996a649528e94bafb69c23f04dcb331595a00173091'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RCEPro::Application.config.session_store :cookie_store, key: '_RCE_pro_session'
