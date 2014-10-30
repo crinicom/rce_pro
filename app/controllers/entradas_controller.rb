19ddc27f66bb789a28c2ea440c6bd8958851ba03
crinicom <cristian.menajovsky@gmail.com>
2014-10-30 16:58:55 -0300

    proyectos listos

--- app/controllers/entradas_controller.rb
+++ app/controllers/entradas_controller.rb
@@ -1,41 +1,68 @@
 class EntradasController < ApplicationController
   #before_action :set_entrada, only: [:show, :edit, :update, :destroy]
-  before_action :load_proyecto
+  #before_action :load_proyecto
 
   # GET /entradas
   # GET /entradas.json
   def index
-    @entradas = Entrada.all
-  end
-
+    proyecto = Proyecto.find(params[:proyecto_id])
+  
+	@entradas = proyecto.entradas
+	
+	respond_to do |format|
+		format.html #index.html.erb
+		format.xml { render :xml => @entradas }
+	end
+	end
+  
   # GET /entradas/1
   # GET /entradas/1.json
   def show
+  proyecto = Proyecto.find(params[:proyecto_id])
+  
+	@entrada = proyecto.entradas.find(params[:id])
+	
+	respond_to do |format|
+		format.html #index.html.erb
+		format.xml { render :xml => @entrada }
+	end
   end
 
   # GET /entradas/new
   def new
-    @entrada = Entrada.new
+    #@entrada = Entrada.new
+	proyecto = Proyecto.find(params[:proyecto_id])
+  
+	@entrada = proyecto.entradas.build
+	
+	respond_to do |format|
+		format.html #index.html.erb
+		format.xml { render :xml => @entrada }
+	end
   end
 
   # GET /entradas/1/edit
   def edit
+	proyecto = Proyecto.find(params[:proyecto_id])
+  
+	@entrada = proyecto.entradas.find(params[:id])
   end
 
   # POST /entradas
   # POST /entradas.json
   def create
     # original: @entrada = Entrada.new(entrada_params)
-	@entrada = @proyecto.entradas.new(entrada_params)
+	proyecto = Proyecto.find(params[:proyecto_id])
+	@entrada = proyecto.entradas.create(entrada_params)
 
 	
     respond_to do |format|
       if @entrada.save
-        format.html { redirect_to @proyecto, notice: 'Entrada was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @proyecto }
+        format.html { redirect_to [@entrada.proyecto, @entrada], notice: 'Entrada was successfully created.' }
+        format.json { render action: 'show', status: :created, location: [@entrada.proyecto,@entrada] }
       else
         format.html { render action: 'new' }
-        format.json { render json: @proyecto.errors, status: :unprocessable_entity }
+        format.json { render json: @entrada.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -43,9 +70,14 @@ class EntradasController < ApplicationController
   # PATCH/PUT /entradas/1
   # PATCH/PUT /entradas/1.json
   def update
-    respond_to do |format|
+    
+	proyecto = Proyecto.find(params[:proyecto_id])
+  
+	@entrada = proyecto.entradas.find(params[:id])
+	
+	respond_to do |format|
       if @entrada.update(entrada_params)
-        format.html { redirect_to @entrada, notice: 'Entrada was successfully updated.' }
+        format.html { redirect_to [@entrada.proyecto,@entrada], notice: 'Entrada was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -57,9 +89,13 @@ class EntradasController < ApplicationController
   # DELETE /entradas/1
   # DELETE /entradas/1.json
   def destroy
-    @entrada.destroy
+    #@entrada.destroy
+	proyecto = Proyecto.find(params[:proyecto_id])
+  
+	@entrada = proyecto.entradas.find(params[:id])
+	@entrada.destroy
     respond_to do |format|
-      format.html { redirect_to entradas_url }
+      format.html { redirect_to proyecto_entradas_url }
       format.json { head :no_content }
     end
   end
--- app/controllers/proyectos_controller.rb
+++ app/controllers/proyectos_controller.rb
@@ -10,6 +10,7 @@ class ProyectosController < ApplicationController
   # GET /proyectos/1
   # GET /proyectos/1.json
   def show
+  @tiempo_tot = @proyecto.entradas.map {|e| e.tiempo}.sum
   end
 
   # GET /proyectos/new
