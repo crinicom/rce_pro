5c52d061ac06250eadab033da63442a8306e4eaf
crinicom <cristian.menajovsky@gmail.com>
2014-12-19 17:27:02 -0300

    corrijo mas cosas para HKU

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ RCEPro::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root :to => "proyectos#index"
-
+  #root :to => "proyectos#index"
+	root :to => "oauth_clients#index"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
